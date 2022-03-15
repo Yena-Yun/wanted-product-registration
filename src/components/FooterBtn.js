e8be933a468e430f262295fa81c9765d7ccab48c
Yena-Yun <cheryleduart@gmail.com>
2022-03-15 21:37:49 +0900

    :fire: FooterBtn의 콘솔 찍는 부분 제거

--- src/components/FooterBtn.js
+++ src/components/FooterBtn.js
@@ -1,36 +1,16 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import { Button, Grid, Text } from 'common';
-import { bg, border } from 'styles/palette';
+import React from "react";
+import { useNavigate } from "react-router";
+import { Button, Grid, Text } from "common";
+import { bg, border } from "styles/palette";
 
 const FooterBtn = (props) => {
   const navigate = useNavigate();
 
-  const footerType = props.type || 'non-first';
-
-  const clickHandler = () => {
-    const argLength = Object.keys(props).length - 2;
-    switch (argLength) {
-      case 0:
-        props.func();
-        break;
-      case 1:
-        props.func(props.arg1);
-        break;
-      case 2:
-        props.func(props.arg1, props.arg2);
-        break;
-      case 5:
-        props.func(props.arg1, props.arg2, props.arg3, props.arg4, props.arg5);
-        break;
-      default:
-        return;
-    }
-  };
+  const footerType = props.type || "non-first";
 
   return (
     <>
-      {footerType === 'first' ? (
+      {footerType === "first" ? (
         <Grid isFlex justify="flex-end" width="auto">
           <Button
             width="10rem"
@@ -39,7 +19,6 @@ const FooterBtn = (props) => {
             bg={bg.primary}
             _onClick={() => {
               navigate(`/${props.params}`);
-              clickHandler();
             }}
           >
             <Text bold color="#fff">
@@ -63,10 +42,7 @@ const FooterBtn = (props) => {
             height="3rem"
             margin="0 1.5rem 0 0"
             bg={bg.primary}
-            _onClick={() => {
-              navigate(`/${props.params}`);
-              clickHandler();
-            }}
+            _onClick={() => navigate(`/${props.params}`)}
           >
             <Text bold color="#fff">
               다음
